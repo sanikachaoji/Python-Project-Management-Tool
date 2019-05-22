bd3cf531d3116beedef6bf37b6782d25be3c8f23
sanikachaoji <csanika@pdx.edu>
2019-05-22 15:35:13 -0700

    Ready for deployment

--- proj_tasks/forms.py
+++ proj_tasks/forms.py
@@ -1,10 +1,11 @@
 from django import forms
 from .models import Task, Project
 class TaskForm(forms.ModelForm):
+
 	class Meta:
 		model = Task
-		fields = ['task_header']
-		labels = {'text': ''}
+		fields = ['task_header', 'task_Description','due_date','task_status', 'project','owner','attachments']
+		labels = {'text':	''}
 		
 class ProjectForm(forms.ModelForm):
 	class Meta:
