7a1a8822d84306cb95277c4fd9fe5a1e164b4102
sanikachaoji <csanika@pdx.edu>
2019-05-22 14:09:51 -0700

    set degug=false for heroku

--- proj_mgmt/settings.py
+++ proj_mgmt/settings.py
@@ -145,7 +145,7 @@ if cwd == '/app' or cwd[:4] == '/tmp':
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     
     # Only allow heroku to host the project.
-    ALLOWED_HOSTS = [".herokuapp.com", ".researchthroughdesign.org"]
+    ALLOWED_HOSTS = ["proj-mgmt-tool.herokuapp.com"]
     DEBUG = False
 
     # Static asset configuration
